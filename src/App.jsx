f4ab3771f92f39f1d8e271b2119a40c7fa72fba5
Dhiraj-Tiwari <rajtiwari73011@gmail.com>
2024-08-16 11:38:15 +0530

    nice

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import Home from './pages/home/Home'
+import AboutUs from './pages/aboutUs/AboutUs'
+import Login from './pages/login/Login'
+import Products from './pages/products/Products'
+import Signup from './pages/signup/Signup'
+import Dashboard from './pages/dashboard/Dashboard'
 
 function App() {
   return (
     <div>
-      app
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/about' element={<AboutUs/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/products' element={<Products/>} />
+          <Route path='/signup' element={<Signup/>} />
+          <Route path='/dashboard' element={<Dashboard/>} />
+        </Routes>
+      </Router>
     </div>
   )
 }
